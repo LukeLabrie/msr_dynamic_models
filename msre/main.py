0a514cca9d039ced5baf30a9b7baffefe26ba1d9
lukelabrie <lplcleary@gmail.com>
2023-08-04 16:17:37 +0200

    working

--- msre/main.py
+++ msre/main.py
@@ -99,19 +99,19 @@ def dydtMSRE(t,y,delays):
 
     # hx precursors 
     if (t > tau_l):
-        dC1dt = beta[0]/Lam-lam[0]*C1-C1/tau_c+C1_delay*np.exp(-lam[0]*tau_l)/tau_c  # C1
-        dC2dt = beta[1]/Lam-lam[1]*C2-C2/tau_c+C2_delay*np.exp(-lam[1]*tau_l)/tau_c  # C2
-        dC3dt = beta[2]/Lam-lam[2]*C3-C3/tau_c+C3_delay*np.exp(-lam[2]*tau_l)/tau_c  # C3
-        dC4dt = beta[3]/Lam-lam[3]*C4-C4/tau_c+C4_delay*np.exp(-lam[3]*tau_l)/tau_c  # C4
-        dC5dt = beta[4]/Lam-lam[4]*C5-C5/tau_c+C5_delay*np.exp(-lam[4]*tau_l)/tau_c  # C5
-        dC6dt = beta[5]/Lam-lam[5]*C6-C6/tau_c+C6_delay*np.exp(-lam[5]*tau_l)/tau_c  # C6
+        dC1dt = n*beta[0]/Lam-lam[0]*C1-C1/tau_c+C1_delay*np.exp(-lam[0]*tau_l)/tau_c  # C1
+        dC2dt = n*beta[1]/Lam-lam[1]*C2-C2/tau_c+C2_delay*np.exp(-lam[1]*tau_l)/tau_c  # C2
+        dC3dt = n*beta[2]/Lam-lam[2]*C3-C3/tau_c+C3_delay*np.exp(-lam[2]*tau_l)/tau_c  # C3
+        dC4dt = n*beta[3]/Lam-lam[3]*C4-C4/tau_c+C4_delay*np.exp(-lam[3]*tau_l)/tau_c  # C4
+        dC5dt = n*beta[4]/Lam-lam[4]*C5-C5/tau_c+C5_delay*np.exp(-lam[4]*tau_l)/tau_c  # C5
+        dC6dt = n*beta[5]/Lam-lam[5]*C6-C6/tau_c+C6_delay*np.exp(-lam[5]*tau_l)/tau_c  # C6
     else:
-        dC1dt = beta[0]/Lam-lam[0]*C1-C1/tau_c+C1*np.exp(-lam[0]*tau_l)/tau_c  # C1
-        dC2dt = beta[1]/Lam-lam[1]*C2-C2/tau_c+C2*np.exp(-lam[1]*tau_l)/tau_c  # C2
-        dC3dt = beta[2]/Lam-lam[2]*C3-C3/tau_c+C3*np.exp(-lam[2]*tau_l)/tau_c  # C3
-        dC4dt = beta[3]/Lam-lam[3]*C4-C4/tau_c+C4*np.exp(-lam[3]*tau_l)/tau_c  # C4
-        dC5dt = beta[4]/Lam-lam[4]*C5-C5/tau_c+C5*np.exp(-lam[4]*tau_l)/tau_c  # C5
-        dC6dt = beta[5]/Lam-lam[5]*C6-C6/tau_c+C6*np.exp(-lam[5]*tau_l)/tau_c  # C6
+        dC1dt = n*beta[0]/Lam-lam[0]*C1-C1/tau_c+C1*np.exp(-lam[0]*tau_l)/tau_c  # C1
+        dC2dt = n*beta[1]/Lam-lam[1]*C2-C2/tau_c+C2*np.exp(-lam[1]*tau_l)/tau_c  # C2
+        dC3dt = n*beta[2]/Lam-lam[2]*C3-C3/tau_c+C3*np.exp(-lam[2]*tau_l)/tau_c  # C3
+        dC4dt = n*beta[3]/Lam-lam[3]*C4-C4/tau_c+C4*np.exp(-lam[3]*tau_l)/tau_c  # C4
+        dC5dt = n*beta[4]/Lam-lam[4]*C5-C5/tau_c+C5*np.exp(-lam[4]*tau_l)/tau_c  # C5
+        dC6dt = n*beta[5]/Lam-lam[5]*C6-C6/tau_c+C6*np.exp(-lam[5]*tau_l)/tau_c  # C6
 
     dydt = [ 
     dTdt_rc_in,                                                                                      # T_in_rc
@@ -119,16 +119,16 @@ def dydtMSRE(t,y,delays):
     -((W_rs/mn_rs)+(hA_rsn/mcp_rsn))*T_out_air + (hA_rsn/mcp_rsn)*T_out_rc + (W_rs/mn_rs)*Trs_in,    # T_out_air
     dTdt_hf_in,                                                                                      # T_in_hf
     -((W_p/mn_p)+(hA_pn/mcp_pn))*T_hf1 + (hA_pn/mcp_pn)*T_ht1 + (W_p/mn_p)*T_in_hf,                  # T_hf1
-    -((W_p/mn_p)+(hA_pn/mcp_pn))*T_hf2 + (hA_pn/mcp_pn)*T_ht1 + (W_p/mn_p)*T_hf1,                    # T_hf2
+    (W_p/mn_p)*(T_hf1-T_hf2) + (hA_pn/mcp_pn)*(T_ht1-T_hf1),                                         # T_hf2
     -((W_p/mn_p)+(hA_pn/mcp_pn))*T_hf3 + (hA_pn/mcp_pn)*T_ht2 + (W_p/mn_p)*T_hf2,                    # T_hf3
-    -((W_p/mn_p)+(hA_pn/mcp_pn))*T_hf4 + (hA_pn/mcp_pn)*T_ht2 + (W_p/mn_p)*T_hf3,                    # T_hf4
+    (W_p/mn_p)*(T_hf3-T_hf4) + (hA_pn/mcp_pn)*(T_ht2-T_hf3),                                         # T_hf4
     -2*((hA_pn/mcp_tn)+(hA_sn/mcp_tn))*T_ht1 + (2*hA_pn/mcp_tn)*T_hf1 + (2*hA_sn/mcp_tn)*T_hc3,      # T_ht1
-    -2*((hA_pn/mcp_tn)+(hA_sn/mcp_tn))*T_ht2 + (2*hA_pn/mcp_tn)*T_hf3 + (2*hA_sn/mcp_tn)*T_hc1,      # T_ht2
+    (2*hA_pn/mcp_tn)*(T_hf3-T_ht2) + (2*hA_sn/mcp_tn)*(T_hc1-T_ht2),      # T_ht2
     dTdt_hc_in,                                                                                      # T_in_hc
     -((W_s/mn_s)+(hA_sn/mcp_sn))*T_hc1 + (hA_sn/mcp_sn)*T_ht2 + (W_s/mn_s)*T_in_hc,                  # T_hc1
-    -((W_s/mn_s)+(hA_sn/mcp_sn))*T_hc2 + (hA_sn/mcp_sn)*T_ht2 + (W_s/mn_s)*T_hc1,                    # T_hc2
+    (W_s/mn_s)*(T_hc1-T_hc2) + (hA_sn/mcp_sn)*(T_ht2-T_hc1),                                         # T_hc2
     -((W_s/mn_s)+(hA_sn/mcp_sn))*T_hc3 + (hA_sn/mcp_sn)*T_ht1 + (W_s/mn_s)*T_hc2,                    # T_hc3
-    -((W_s/mn_s)+(hA_sn/mcp_sn))*T_hc4 + (hA_sn/mcp_sn)*T_ht1 + (W_s/mn_s)*T_hc3,                    # T_hc4
+    (W_s/mn_s)*(T_hc3-T_hc4) + (hA_sn/mcp_sn)*(T_ht1-T_hc3),                                         # T_hc4
     dTdt_cf_in,                                                                                      # T_in_cf
     (rho-beta_t)*n/Lam+lam[0]*C1+lam[1]*C2+lam[2]*C3+lam[3]*C4+lam[4]*C5+lam[5]*C6,                  # n (no source insertion)
     dC1dt,                                                                                           # C1
@@ -139,7 +139,7 @@ def dydtMSRE(t,y,delays):
     dC6dt,                                                                                           # C6
     (hA_fg/mcp_g1)*(T_cf1 - T_cg) + k_g*P*n/mcp_g1,                                                  # T_cg
     W_f/mn_f*(T_in_cf - T_cf1) + (k_f1*P*n/mcp_f1) + (hA_fg*k_1*(T_cg - T_cf1)/mcp_f1),              # T_cf1   
-    W_f/mn_f*(T_cf1 - T_cf2) + (k_f2*P*n/mcp_f2) + (hA_fg*k_2*(T_cg - T_cf2)/mcp_f2)                 # T_cf2   
+    W_f/mn_f*(T_cf1 - T_cf2) + (k_f2*P*n/mcp_f2) + (hA_fg*k_2*(T_cg - T_cf1)/mcp_f2)                 # T_cf2   
     ]
     return dydt
 
@@ -158,8 +158,9 @@ def get_delay_indicies(t_current,timeVec):
         i_min = 0
         diff_min = math.inf
         for t in enumerate(timeVec):
-            if (t[1]-t_d < diff_min):
+            if (t[1]-t_current < diff_min):
                 i_min = t[0]
+                diff_min = t[1]-t_d
         
         d_indicies[0] = i_min # radiator coolant outlet
         d_indicies[1] = i_min # radiator air outlet 
@@ -175,8 +176,9 @@ def get_delay_indicies(t_current,timeVec):
         i_min = 0
         diff_min = math.inf
         for t in enumerate(timeVec):
-            if (t[1]-t_d < diff_min):
+            if (t[1]-t_current < diff_min):
                 i_min = t[0]
+                diff_min = t[1]-t_d
         
         d_indicies[3] = i_min # hx coolant outlet (coolant node 3)
         d_indicies[4] = i_min # hx coolant outlet (coolant node 4)
@@ -192,8 +194,9 @@ def get_delay_indicies(t_current,timeVec):
         i_min = 0
         diff_min = math.inf
         for t in enumerate(timeVec):
-            if (t[1]-t_d < diff_min):
+            if (t[1]-t_current < diff_min):
                 i_min = t[0]
+                diff_min = t[1]-t_d
         
         d_indicies[6] = i_min # hx fuel outlet (tube node 2)
         d_indicies[11] = i_min # hx fuel outlet (fuel node 3)
@@ -209,8 +212,9 @@ def get_delay_indicies(t_current,timeVec):
         i_min = 0
         diff_min = math.inf
         for t in enumerate(timeVec):
-            if (t[1]-t_d < diff_min):
+            if (t[1]-t_current < diff_min):
                 i_min = t[0]
+                diff_min = t[1]-t_d
         
         d_indicies[7] = i_min # core fuel outlet (fuel node 1)
         d_indicies[8] = i_min # core fuel outlet (fuel node 2)
@@ -225,10 +229,11 @@ def get_delay_indicies(t_current,timeVec):
 
         # find closest value
         i_min = 0
-        diff_min = math.inf
+        diff_min = 9999.99999
         for t in enumerate(timeVec):
-            if (t[1]-t_d < diff_min):
+            if (abs(t[1]-t_current) < diff_min):
                 i_min = t[0]
+                diff_min = abs(t[1]-t_d)
         
         d_indicies[13] = i_min # precursor group 1
         d_indicies[14] = i_min # precursor group 2
@@ -240,11 +245,22 @@ def get_delay_indicies(t_current,timeVec):
     return d_indicies
 
 
+def get_tIdx(t,tao,timeVec):
+    td = t-tao
+    diff_min = 999999.9999999
+    idx = 0
+    for t in enumerate(timeVec):
+        diff = abs(td-t[1])
+        if (diff<diff_min):
+            diff_min = abs(td-t[1])
+            idx = t[0]
+    return idx
+
+
 
 def main():
 
     # initial time
-    t0 = 0.0
 
     # initial conditions
     y0 = [T0_s4, T0_rp, T0_rs, T0_f2, T0_p1,T0_p2, T0_p3, T0_p4, T0_t1, T0_t2,
@@ -267,10 +283,12 @@ def main():
         sol_interim.append([t, *y])
     r.set_solout(solout)
 
+    t0 = 0.0
+    t_start = t0
     sol = []
-    k = 15
-    t_start = 0.0
-    t_stop = 30.0
+    y_next = []
+    k = 2
+    t_stop = 250.0
     d_new = [0]*19
     i = 0
     while (t_start < t_stop):
@@ -282,9 +300,34 @@ def main():
             sol.append(sol_interim[1])
         else:
             t_start = sol[-1][0]
-            r.set_initial_value(sol[-1][1:],t_start).set_f_params(d_new)
+            r.set_initial_value(y_next,t_start).set_f_params(d_new)
             r.integrate(t_start+1.0)
             sol.append(sol_interim[1])
+
+        # account for delays 
+        # core fuel inlet
+        idx_cf_in = 0
+        if (t_start > tau_hx_c):
+            idx_cf_in = get_tIdx(t_start,tau_hx_c,[s[0] for s in sol])
+        sol[-1][16] = sol[idx_cf_in][8]
+
+        # heat exchanger fuel inlet
+        idx_hf_in = 0
+        if (t_start > tau_c_hx):
+            idx_hf_in = get_tIdx(t_start,tau_c_hx,[s[0] for s in sol])
+        sol[-1][4] = sol[idx_hf_in][26]
+
+        # heat exchanger coolant inlet
+        idx_hc_in = 0
+        if (t_start > tau_r_hx):
+            idx_hc_in = get_tIdx(t_start,tau_r_hx,[s[0] for s in sol])
+        sol[-1][11] = sol[idx_hc_in][2]
+
+        # radiator coolant inlet
+        idx_rc_in = 0
+        if (t_start > tau_hx_r):
+            idx_rc_in = get_tIdx(t_start,tau_hx_r,[s[0] for s in sol])
+        sol[-1][1] = sol[idx_rc_in][15]
 
         # update delay terms 
         d_idx = get_delay_indicies(t_start,[s[0] for s in sol])
@@ -293,10 +336,12 @@ def main():
         y_to_d_map = [1,2,14,13,14,8,9,24,25,23,16,6,7,17,18,19,20,21,22]
         for j in range(19):
             d_new[j] = sol[d_idx[j]][y_to_d_map[j]+1] 
+
+        y_next = sol[-1][1:]
         sol_interim = []
         i += 1
 
-    for i in range(k-5,k):
+    for i in range(len(sol)-6,len(sol)-1):
         i_rev = len(sol)-(10-i)
         print(f"t: {sol[i][0]}")
         print(f"n: {sol[i][17]}")
@@ -312,11 +357,43 @@ def main():
         T_cf2 = sol[i][26]
         print(f"rho: {(a_f/2)*((-T0_f1+T_cf1)+(-T0_f2+T_cf2)) + a_g*(-T0_g1+T_cg)}\n")
 
-    rhos = [(a_f/2)*((-T0_f1+sol[i][25])+(-T0_f2+sol[i][26])) + a_g*(-T0_g1+sol[i][24]) for i in range(len(sol))]
-    plt.plot([s[0] for s in sol],[s[17] for s in sol])
-    
-    #plt.plot([s[0] for s in sol],rhos)
+    print(sol[-5:])
+    #rhos = [(a_f/2)*((-T0_f1+sol[i][25])+(-T0_f2+sol[i][26])) + a_g*(-T0_g1+sol[i][24]) for i in range(len(sol))]
+
+    #for j in range(len(sol[0])):
+    #    plt.plot([s[0] for s in sol],[s[j] for s in sol])
+    #    plt.show()
+    # 1: radiatior coolant inlet temp
+    # 2: radiator coolant outlet temp
+    # 3: radiator air outlet temp
+    # 4: heat exchanger fuel inlet temp
+    # 5: heat exchanger fuel node 1 temp
+    # 6: heat exchanger fuel node 2 temp
+    # 7: heat exchanger fuel node 3 temp
+    # 8: heat exchanger fuel node 4 temp
+    # 9: heat exchanger tube node 1 temp
+    # 10: heat exchanger tube node 2 temp
+    # 11: heat exchanger coolant inlet temp
+    # 12: heat exchanger coolant node 1
+    # 13: heat exchanger coolant node 2
+    # 14: heat exchanger coolant node 3
+    # 15: heat exchanger coolant node 4
+    # 16: core fuel inlet temp
+    # 17: k
+    # 18: C1
+    # 19: C2
+    # 20: C3
+    # 21: C4
+    # 22: C5
+    # 23: C6
+    # 24: core graphite temp
+    # 25: core fuel node 1 temp
+    # 26: core fuel node 2 temp
+    of_interest = 17
+    plt.plot([s[0] for s in sol],[s[of_interest] for s in sol])
     plt.show()
+    #for i in range(len(sol)):
+    #    print(f"t: {sol[i][0]}, hx out: {sol[i][8]}, core in: {sol[i][16]}")
 
     return None 
 
@@ -382,7 +459,8 @@ def debug2():
           C0[5]] 
 
     dydt = dydtMSRE(t0,y0,d0)
-    print(dydt)
+    for i in range(len(dydt)):
+        print(f"{y0[i]},{dydt[i]}")
 
     return None
 
